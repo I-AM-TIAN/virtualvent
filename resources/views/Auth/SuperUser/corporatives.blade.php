ea0758f65c428804be676ed343a13a4f9da55983
mariab432 <maria.alvisb@campusucc.edu.co>
2024-05-20 11:35:14 -0500

    registro modificacion corporativo

--- resources/views/Auth/SuperUser/corporatives.blade.php
+++ resources/views/Auth/SuperUser/corporatives.blade.php
@@ -105,7 +105,8 @@
                                                 </td>
                                                 <td class="py-4 px-6 text-sm font-medium whitespace-nowrap">
                                                     <a href="#"
-                                                        class="text-blue-600 dark:text-blue-500 hover:underline" data-modal-toggle="authentication-modal">Edit</a>
+                                                        class="text-blue-600 dark:text-blue-500 hover:underline"
+                                                        data-modal-toggle="authentication-modal">Edit</a>
                                                 </td>
                                             </tr>
                                         @endforeach
@@ -209,46 +210,67 @@
                                     </button>
                                 </div>
                                 <form class="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8" action="#">
-                                    <h3 class="text-xl font-medium text-gray-900 dark:text-white">Modificar usuario corporativo</h3>
+                                    <h3 class="text-xl font-medium text-gray-900 dark:text-white">Modificar usuario
+                                        corporativo</h3>
                                     <div>
-                                        <label for="email"
-                                            class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Your
-                                            email</label>
-                                        <input type="email" name="email" id="email"
+                                        <label for="nit"
+                                            class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Nit</label>
+                                        <input type="number" name="nit" id="nit"
                                             class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                                            placeholder="name@company.com" required="">
+                                            placeholder="258" required="">
                                     </div>
                                     <div>
-                                        <label for="password"
-                                            class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Your
-                                            password</label>
-                                        <input type="password" name="password" id="password" placeholder="••••••••"
+                                        <label for="razonsocial"
+                                            class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Razon
+                                            social
+                                        </label>
+                                        <input type="text" name="razonsocial" id="razonsocial"
+                                            placeholder="Marmola"
                                             class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                             required="">
                                     </div>
-                                    <div class="flex justify-between">
-                                        <div class="flex items-start">
-                                            <div class="flex items-center h-5">
-                                                <input id="remember" aria-describedby="remember" type="checkbox"
-                                                    class="bg-gray-50 border border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                                                    required="">
-                                            </div>
-                                            <div class="text-sm ml-3">
-                                                <label for="remember"
-                                                    class="font-medium text-gray-900 dark:text-gray-300">Remember
-                                                    me</label>
-                                            </div>
-                                        </div>
-                                        <a href="#"
-                                            class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost
-                                            Password?</a>
+                                    <div>
+                                        <label for="email"
+                                            class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Correo
+                                            electronico
+                                        </label>
+                                        <input type="email" name="email" id="email"
+                                            placeholder="example@example.com"
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                            required="">
                                     </div>
-                                    <button type="submit"
-                                        class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login
-                                        to your account</button>
-                                    <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                                        Not registered? <a href="#"
-                                            class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+                                    <div>
+                                        <label for="nombreusuario"
+                                            class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Nombre
+                                            usuario
+                                        </label>
+                                        <input type="text" name="nombreusuario" id="nombreusuario"
+                                            placeholder="Marmo.la"
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                            required="">
+                                    </div>
+                                    <div>
+                                        <label for="telefono"
+                                            class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Número
+                                            de contacto
+                                        </label>
+                                        <input type="number" name="telefono" id="telefono"
+                                            placeholder="3*********"
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                            required="">
+                                    </div>
+                                    <div>
+                                        <label for="direccion"
+                                            class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Direccion
+                                        </label>
+                                        <input type="text" name="direccion" id="direccion" placeholder="Marmola"
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                            required="">
+                                    </div>
+                                    <div>
+                                        <button type="submit"
+                                            class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Modificar
+                                            </button>
                                     </div>
                                 </form>
                             </div>
